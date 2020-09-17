e07a7425950dae54eca90ce69eb0b0e38f6fcf5b
J K <kj688895@gmail.com>
2020-09-17 00:44:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,2 +1,5 @@
 def batch_badge_creator(name)
   badge_maker = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  badge_maker.each do {|name| puts "Hello, my name is #{name}."
+  end
+end
